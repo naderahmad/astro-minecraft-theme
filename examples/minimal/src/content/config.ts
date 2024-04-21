dfce4d44b36016b2a4f2fb38ac34603faf987f2e
Bryce Russell <brycetrussell@gmail.com>
2024-04-20 21:25:49 -0500

    Update docs and minimal example
    Temporarily removed collection configs until astro-theme-provider supports shema injection

--- examples/minimal/src/content/config.ts
+++
@@ -1,6 +0,0 @@
-import { defineCollection } from 'astro:content';
-import AstroCraftContentSchema from 'astrocraft/schema';
-
-export const collections = {
-  pages: defineCollection({ schema: AstroCraftContentSchema() }),
-};
