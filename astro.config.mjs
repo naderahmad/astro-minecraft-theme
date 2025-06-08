6233527c833310cfebb4072e85d9def35924a8a6
Nader Ahmad <naderahmad7x@gmail.com>
2025-06-08 00:01:53 -0400

    sdsd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:  'https://naderahmad.github.io/astro-minecraft-theme/',
+  base:  '/astro-minecraft-theme/',
   integrations: [
     tailwind({
       config: { applyBaseStyles: false },
